1fe15764865b65168e5c93ea29b8d802789d821c
Erick Christian <erickchristian48@gmail.com>
2024-12-20 10:11:03 +0700

    test env

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
+import type { Route } from "./+types/home"
+
+export function loader() {
+  return { env: process.env }
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  console.log(loaderData)
+
   return <h1>title</h1>
 }
